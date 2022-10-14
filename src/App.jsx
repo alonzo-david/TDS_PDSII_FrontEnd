0e8c5bb8a5580927335f968a3e3fce11d4bb85f5
david-alonzo <78769397+alonzo-david@users.noreply.github.com>
2022-10-13 21:07:44 -0600

    configuraciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import Routes from "./Routes";
 import Header from "./components/Header/Header";
-import Footer from './components/Footer/Footer';
+import Footer from "./components/Footer/Footer";
+
+import './App.css';
 
 class App extends Component {
   constructor(props) {
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
-/*import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';*/
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./App";
-import reportWebVitals from './reportWebVitals';
-
-/*ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);*/
-
+// import reportWebVitals from './reportWebVitals';
 
 const App = () => (
   <Router>
