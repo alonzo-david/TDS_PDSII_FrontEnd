7cac061db66552a99cd2c709bc5c9740a3b5323c
david-alonzo <78769397+alonzo-david@users.noreply.github.com>
2022-10-14 22:14:21 -0600

    Se agrega validacion para que muestre el componente de Dashboard o Welcome

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { withRouter, useHistory } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import Welcome from "../Welcome/Welcome";
 
 import "./Home.css";
-import Login from "../Login/Login";
-import Partida from "../Partida/Partida";
 
 const Home = (props) => {
   const [data, setData] = useState([]);
   const [open, setOpen] = useState(false);
   const [newProduct, setNewProduct] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
   const history = useHistory();
 
   useEffect(() => {
@@ -20,8 +21,7 @@ const Home = (props) => {
 
   return (
     <div className="height-body" style={{ backgroundColor: "#EFEFEF" }}>
-      {/* <Login></Login> */}
-      <Partida></Partida>
+      {isLogin ? <Dashboard /> : <Welcome />}
     </div>
   );
 };
