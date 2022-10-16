9e009b3715aab0c152f0f57ab6f284c140e7546c
david-alonzo <78769397+alonzo-david@users.noreply.github.com>
2022-10-16 00:17:10 -0600

    Se empiezan a desarrollar los componentes de partidas y preguntas

--- src/components/Pregunta/Pregunta.jsx
+++ src/components/Pregunta/Pregunta.jsx
@@ -1,21 +1,192 @@
-import React, { useEffect } from "react";
-import { Api, ApiPregunta } from "./../../services/Api";
+import React from "react";
+import {
+  Box,
+  ButtonBase,
+  Container,
+  CssBaseline,
+  Typography,
+} from "@mui/material";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
+
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme({
+  customColor: {
+    answer: "#4D8E17",
+  },
+});
+
+const images = [
+  {
+    url: "./img/q2.png",
+    title: "Breakfast",
+    width: "30%",
+  },
+  {
+    url: "./img/q2.png",
+    title: "Burgers",
+    width: "30%",
+  },
+  {
+    url: "./img/q2.png",
+    title: "Camera",
+    width: "30%",
+  },
+];
+
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: "relative",
+  height: 200,
+  [theme.breakpoints.down("sm")]: {
+    width: "100% !important", // Overrides inline-style
+    height: 100,
+  },
+  "&:hover, &.Mui-focusVisible": {
+    zIndex: 1,
+    "& .MuiImageBackdrop-root": {
+      opacity: 0.15,
+    },
+    "& .MuiImageMarked-root": {
+      opacity: 0,
+    },
+    "& .MuiTypography-root": {
+      border: "4px solid currentColor",
+    },
+  },
+}));
+
+const ImageSrc = styled("span")({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: "cover",
+  backgroundPosition: "center 40%",
+});
+
+const Image = styled("span")(({ theme }) => ({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: theme.customColor.answer,
+}));
+
+const ImageBackdrop = styled("span")(({ theme }) => ({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create("opacity"),
+}));
+
+const ImageMarked = styled("span")(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: "absolute",
+  bottom: -2,
+  left: "calc(50% - 9px)",
+  transition: theme.transitions.create("opacity"),
+}));
+
+
+
+
+
+
 
 const Pregunta = () => {
-  useEffect(() => {
-    getPregunta();
-  }, []);
-
-  const getPregunta = () => {
-    ApiPregunta.Get("/preguntas.php?grupo=6&nivel=1")
-      .then((res) => {
-        console.log("Result Auth: ", res);
-      })
-      .catch((ex) => {
-        console.error("error", ex);
-      });
-  };
-
-  return <div></div>;
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="md">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Album layout
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don&apos;t simply skip over it entirely.
+            </Typography>
+            <Container sx={{ py: 8 }} maxWidth="md">
+              <Box
+                sx={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  minWidth: 300,
+                  width: "100%",
+                }}
+              >
+                {images.map((image) => (
+                  <ImageButton
+                    focusRipple
+                    key={image.title}
+                    style={{
+                      width: image.width,
+                      marginRight: 15,
+                    }}
+                  >
+                    <ImageSrc
+                      style={{ backgroundImage: `url(${image.url})` }}
+                    />
+                    <ImageBackdrop className="MuiImageBackdrop-root" />
+                    <Image>
+                      <Typography
+                        component="span"
+                        variant="subtitle1"
+                        color="inherit"
+                        sx={{
+                          position: "relative",
+                          p: 4,
+                          pt: 2,
+                          pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                        }}
+                      >
+                        {image.title}
+                        <ImageMarked className="MuiImageMarked-root" />
+                      </Typography>
+                    </Image>
+                  </ImageButton>
+                ))}
+              </Box>
+            </Container>
+          </Container>
+        </Box>        
+      </main>      
+    </ThemeProvider>
+  );
 };
 export default Pregunta;
--- src/view/Partida/Partida.jsx
+++ src/view/Partida/Partida.jsx
@@ -1,21 +1,177 @@
-import React, { useEffect } from "react";
-import Pregunta from "../../components/Pregunta/Pregunta";
+import React, { useEffect, Fragment } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+} from "@mui/material";
+import { Api, ApiPregunta } from "./../../services/Api";
 
 import "./Partida.css";
+import Pregunta from "../../components/Pregunta/Pregunta";
 
-
+const steps = [
+  "Select campaign settings",
+  "Create an ad group",
+  "Create an ad",
+];
 
 const Partida = () => {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+
   useEffect(() => {
     console.log("useEffect Partida");
+
+    getTipoUsuarios();
+    //getPreguntas();
   }, []);
 
-  
+  const getPreguntas = () => {
+    ApiPregunta.Get("/preguntas.php?grupo=6&nivel=1")
+      .then((res) => {
+        console.log("Result Auth: ", res);
+      })
+      .catch((ex) => {
+        console.error("error", ex);
+      });
+  };
+
+  const getTipoUsuarios = () => {
+    Api.Get("/tipousuario")
+      .then((res) => {
+        console.log("Result Auth: ", res);
+      })
+      .catch((ex) => {
+        console.error("error", ex);
+      });
+  };
+
+  /***
+   *
+   * INICIO STEPPER
+   */
+
+  const isStepOptional = (step) => {
+    return step === 1;
+  };
+
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  /**
+   * FIN STEPPER
+   */
 
   return (
-    <div>
-      <h1>Partida</h1>
-    </div>
+    <Container maxWidth="md">
+      <Box sx={{ 
+        width: "100%" ,
+
+        bgcolor: "background.paper",
+            mt: 8,
+            pb: 6,
+        }}
+      >
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
+            if (isStepOptional(index)) {
+              labelProps.optional = (
+                <Typography variant="caption">Optional</Typography>
+              );
+            }
+            if (isStepSkipped(index)) {
+              stepProps.completed = false;
+            }
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Box sx={{ flex: "1 1 auto" }} />
+              <Button onClick={handleReset}>Reset</Button>
+            </Box>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              
+              Step {activeStep + 1}
+              <Pregunta />
+
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+              <Box sx={{ flex: "1 1 auto" }} />
+              {/* {isStepOptional(activeStep) && (
+              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+                Skip
+              </Button>
+            )} */}
+
+              <Button onClick={handleNext}>
+                {activeStep === steps.length - 1 ? "Finish" : "Next"}
+              </Button>
+            </Box>
+          </Fragment>
+        )}
+      </Box>
+    </Container>
   );
 };
 
