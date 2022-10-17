48a26efe9ec24db69c58911d559a47713bd52031
david-alonzo <78769397+alonzo-david@users.noreply.github.com>
2022-10-16 23:41:04 -0600

    Se desarrolla modulo de partida consumiento el api proporcionado por el catedratico y configuracion del modulo

--- src/view/Partida/Partida.jsx
+++ src/view/Partida/Partida.jsx
@@ -1,4 +1,10 @@
-import React, { useEffect, Fragment } from "react";
+import React, {
+  useEffect,
+  Fragment,
+  useCallback,
+  useState,
+  useLayoutEffect,
+} from "react";
 import {
   Box,
   Button,
@@ -7,6 +13,10 @@ import {
   Step,
   StepLabel,
   Typography,
+  Grid,
+  CardActionArea,
+  CardContent,
+  Card,
 } from "@mui/material";
 import { Api, ApiPregunta } from "./../../services/Api";
 
@@ -14,24 +24,38 @@ import "./Partida.css";
 import Pregunta from "../../components/Pregunta/Pregunta";
 
 const steps = [
-  "Select campaign settings",
-  "Create an ad group",
-  "Create an ad",
+  "Nivel 1",
+  "Nivel 2",
+  "Nivel 3",
+  "Nivel 4",
+  "Nivel 5",
+  "Nivel 6",
+  "Nivel 7",
+  "Nivel 8",
+  "Nivel 9",
+  "Nivel 10",
 ];
 
 const Partida = () => {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
+  const [activeStep, setActiveStep] = useState(0);
+  const [skipped, setSkipped] = useState(new Set());
+  const [isCorrect, setIsCorrect] = useState(null);
+  const [isChecked, setIsChecked] = useState(false);
+  const [noPregunta, setNoPregunta] = useState(1);
+  const [cardSelected, setCardSelected] = useState(null);
+  const [preguntas, setPreguntas] = useState([]);
 
   useEffect(() => {
     console.log("useEffect Partida");
-
-    getTipoUsuarios();
-    //getPreguntas();
   }, []);
 
-  const getPreguntas = () => {
-    ApiPregunta.Get("/preguntas.php?grupo=6&nivel=1")
+  useEffect(() => {
+    console.log("Partida No. Pregunta: ", noPregunta);
+    getPreguntas(noPregunta);
+  }, [noPregunta]);
+
+  const getTipoUsuarios = () => {
+    Api.Get("/tipousuario")
       .then((res) => {
         console.log("Result Auth: ", res);
       })
@@ -40,10 +64,13 @@ const Partida = () => {
       });
   };
 
-  const getTipoUsuarios = () => {
-    Api.Get("/tipousuario")
+  const getPreguntas = (_noPregunta) => {
+    ApiPregunta.Get("/preguntas.php?grupo=6&nivel=" + _noPregunta)
       .then((res) => {
-        console.log("Result Auth: ", res);
+        // console.log("Result Auth: ", res.data.json());
+        const result = Object.values(res.data);
+        console.log("Result Auth: ", result);
+        setPreguntas(result);
       })
       .catch((ex) => {
         console.error("error", ex);
@@ -61,6 +88,10 @@ const Partida = () => {
 
   const isStepSkipped = (step) => {
     return skipped.has(step);
+  };
+
+  const isStepFailed = (step) => {
+    return step === 1;
   };
 
   const handleNext = () => {
@@ -101,24 +132,103 @@ const Partida = () => {
    * FIN STEPPER
    */
 
-  return (
-    <Container maxWidth="md">
-      <Box sx={{ 
-        width: "100%" ,
+  const handleAnswered = (_isCorrect, _cardSelected) => {
+    const isCorrect = _isCorrect === 1 ? true : false;
 
-        bgcolor: "background.paper",
-            mt: 8,
-            pb: 6,
+    setIsCorrect(isCorrect);
+    setCardSelected(_cardSelected);
+  };
+
+  const handleChecked = () => {
+    if (isCorrect === null) {
+
+    } else {
+      setIsChecked(true);
+    }
+  }
+
+  const handleContinue = () => {
+    handleNext();
+    if (noPregunta < 10) {
+      setNoPregunta(noPregunta + 1);
+    } else {
+      setNoPregunta(1);
+      handleReset();
+    }
+
+    setIsChecked(false);
+    setCardSelected(null);
+  }
+
+  const handleRetry = () => {
+    setCardSelected(null);
+    setIsCorrect(null);
+    setIsChecked(false);
+  }
+  // return (
+  //   <Container maxWidth="md">
+  //     <Typography
+  //       component="h1"
+  //       variant="h2"
+  //       align="center"
+  //       color="text.primary"
+  //       gutterBottom
+  //     >
+  //       Trivia Guatemala
+  //     </Typography>
+  //     <Typography variant="h5" align="center" color="text.secondary" paragraph>
+  //       {preguntas[0]}
+  //     </Typography>
+  //     <Grid container spacing={4}>
+  //       {preguntas.map((res, i) => {
+  //         if (i > 0) {
+  //           return (
+  //             <Grid item md={4} key={i}>
+  //               <Card
+  //                 sx={{ maxWidth: 345 }}
+  //                 onClick={() => {
+  //                   handleAnswered(res.is_correct);
+  //                 }}
+  //               >
+  //                 <CardActionArea sx={{ height: 200 }}>
+  //                   <CardContent>
+  //                     <Typography gutterBottom variant="h5" component="div">
+  //                       {res.text}
+  //                     </Typography>
+  //                   </CardContent>
+  //                 </CardActionArea>
+  //               </Card>
+  //             </Grid>
+  //           );
+  //         }
+  //       })}
+  //     </Grid>
+  //   </Container>
+  // );
+
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          width: "100%",
+
+          bgcolor: "background.paper",
+          mt: 8,
+          pb: 6,
         }}
       >
         <Stepper activeStep={activeStep}>
           {steps.map((label, index) => {
             const stepProps = {};
             const labelProps = {};
-            if (isStepOptional(index)) {
+            if (isChecked && !isCorrect && index == noPregunta - 1) {
               labelProps.optional = (
-                <Typography variant="caption">Optional</Typography>
+                <Typography variant="caption" color="error">
+                  Respuesta incorrecta
+                </Typography>
               );
+
+              labelProps.error = true;
             }
             if (isStepSkipped(index)) {
               stepProps.completed = false;
@@ -143,31 +253,85 @@ const Partida = () => {
         ) : (
           <Fragment>
             <Typography sx={{ mt: 2, mb: 1 }}>
-              
               Step {activeStep + 1}
-              <Pregunta />
-
+              <Container maxWidth="md">
+                <Typography
+                  component="h1"
+                  variant="h2"
+                  align="center"
+                  color="text.primary"
+                  gutterBottom
+                >
+                  Trivia Guatemala
+                </Typography>
+                <Typography
+                  variant="h5"
+                  align="center"
+                  color="text.secondary"
+                  paragraph
+                >
+                  {preguntas[0]}
+                </Typography>
+                <Grid container spacing={4}>
+                  {preguntas.map((res, i) => {
+                    if (i > 0) {
+                      return (
+                        <Grid item md={4} key={i}>
+                          <Card
+                            sx={{ maxWidth: 345 }}
+                            onClick={() => {
+                              handleAnswered(res.is_correct, i);
+                            }}
+                            key={i}
+                            style={cardSelected == i ? { backgroundColor: "#4D8E17", color: "#FFFFFF" } : {}}
+                          >
+                            <CardActionArea sx={{ height: 200 }}>
+                              <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                  {res.text}
+                                </Typography>
+                              </CardContent>
+                            </CardActionArea>
+                          </Card>
+                        </Grid>
+                      );
+                    }
+                  })}
+                </Grid>
+                <Grid
+                  container
+                  alignItems="center"
+                  justifyContent="center"
+                  direction="column"
+                >
+                  <Grid item md={12}>
+                    {
+                      isChecked && isCorrect && (
+                        <label style={{ color: "#4D8E17", fontSize: 25 }}>Su respuesta es correcta</label>
+                      )
+                    }
+                    {
+                      isChecked && !isCorrect && (
+                        <label style={{ color: "#FF0000", fontSize: 25 }}>Su respuesta es incorrecta</label>
+                      )
+                    }
+                  </Grid>
+                  <Grid item md={12}>
+                    {
+                      isChecked ? (
+                        isCorrect ? (
+                          <Button variant="contained" style={{ backgroundColor: "#4D8E17" }} onClick={handleContinue}>Continuar</Button>
+                        ) : (
+                          <Button variant="contained" style={{ backgroundColor: "#4D8E17" }} onClick={handleRetry}>Reintentar</Button>
+                        )
+                      ) : (
+                        <Button variant="contained" style={{ backgroundColor: "#4D8E17" }} onClick={handleChecked}>Comprobar</Button>
+                      )
+                    }
+                  </Grid>
+                </Grid>
+              </Container>
             </Typography>
-            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-              <Button
-                color="inherit"
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                sx={{ mr: 1 }}
-              >
-                Back
-              </Button>
-              <Box sx={{ flex: "1 1 auto" }} />
-              {/* {isStepOptional(activeStep) && (
-              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                Skip
-              </Button>
-            )} */}
-
-              <Button onClick={handleNext}>
-                {activeStep === steps.length - 1 ? "Finish" : "Next"}
-              </Button>
-            </Box>
           </Fragment>
         )}
       </Box>
